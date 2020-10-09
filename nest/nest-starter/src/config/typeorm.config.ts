89d131a9357c948431845ce5722b2a5f9860188a
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-10 00:06:58 +0330

    :construction: wip: add entity config in typeorm

--- nest/nest-starter/src/config/typeorm.config.ts
+++ nest/nest-starter/src/config/typeorm.config.ts
@@ -10,8 +10,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   // username: process.env.DB_USER || dbConfig.username,
   // password: process.env.DB_PASSWORD || dbConfig.password,
   // database: process.env.DB_NAME || dbConfig.database,
-  // entities: [__dirname + '/../**/*.entity.{js,ts}'],
   // synchronize: process.env.DB_SYNCHRONIZE || dbConfig.synchronize,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   url: process.env.DATABASE_URL,
   synchronize: true
 };
