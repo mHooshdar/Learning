efa948a10544a96198766a16c66f62e8eae3c9a8
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-09 23:57:56 +0330

    :construction: wip: uncomment db type

--- nest/nest-starter/src/config/typeorm.config.ts
+++ nest/nest-starter/src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ import * as config from 'config';
 const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  // type: process.env.DB_TYPE ||dbConfig.type,
+  type: process.env.DB_TYPE ||dbConfig.type,
   // host: process.env.DB_HOST || dbConfig.host,
   // port: process.env.DB_PORT || dbConfig.port,
   // username: process.env.DB_USER || dbConfig.username,
