7c05d82bd382f42086f004c07921271a2c81ba0b
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-10 00:11:09 +0330

    :construction: wip: fix the jwt config

--- nest/nest-starter/src/auth/jwt.strategy.ts
+++ nest/nest-starter/src/auth/jwt.strategy.ts
@@ -17,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
     });
   }
 
