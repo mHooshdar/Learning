fcb29a0ca2f30789925553047bd2345a92527411
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-09 23:51:54 +0330

    :construction: wip: change typeorm config

--- nest/nest-starter/src/config/typeorm.config.ts
+++ nest/nest-starter/src/config/typeorm.config.ts
@@ -4,12 +4,13 @@ import * as config from 'config';
 const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: process.env.DB_TYPE ||dbConfig.type,
-  host: process.env.DB_HOST || dbConfig.host,
-  port: process.env.DB_PORT || dbConfig.port,
-  username: process.env.DB_USER || dbConfig.username,
-  password: process.env.DB_PASSWORD || dbConfig.password,
-  database: process.env.DB_NAME || dbConfig.database,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: process.env.DB_SYNCHRONIZE || dbConfig.synchronize,
+  // type: process.env.DB_TYPE ||dbConfig.type,
+  // host: process.env.DB_HOST || dbConfig.host,
+  // port: process.env.DB_PORT || dbConfig.port,
+  // username: process.env.DB_USER || dbConfig.username,
+  // password: process.env.DB_PASSWORD || dbConfig.password,
+  // database: process.env.DB_NAME || dbConfig.database,
+  // entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  // synchronize: process.env.DB_SYNCHRONIZE || dbConfig.synchronize,
+  url: process.env.DATABASE_URL,
 };
