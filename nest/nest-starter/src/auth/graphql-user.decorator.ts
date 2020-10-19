a65a37c0aa1f761d4596bc5fd343b1e6b4d8874b
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-20 00:37:19 +0330

    :sparkles: feat: add graphql auth config

---
+++ nest/nest-starter/src/auth/graphql-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req.user;
+  },
+);
+
+// use in the appp module
+// GraphQLModule.forRoot({
+//   context: ({ req }) => ({ req }),
+// });
