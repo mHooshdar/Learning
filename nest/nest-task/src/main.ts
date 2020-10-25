dd89ca697ed7d5fc2b2f853b87e2ee8d6623753f
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-26 01:19:51 +0330

    :lock: feat: add security packages like csurf and helmet

--- nest/nest-task/src/main.ts
+++ nest/nest-task/src/main.ts
@@ -2,8 +2,12 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as config from 'config';
 import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 const serverConfig = config.get('server');
 
@@ -11,17 +15,25 @@ async function bootstrap() {
   const port = process.env.PORT || serverConfig.port;
   const logger = new Logger('bootstrap');
 
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
+  app.use(helmet());
   app.use(compression());
-
+  app.use(csurf());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
-
   await app.listen(port);
+
   logger.log(`Aplication listening on port ${port}`);
 }
 bootstrap();
